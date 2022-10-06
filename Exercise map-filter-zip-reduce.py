ba01e9bac0296b6dc7adaa5ff1b2b5c964438f3d
GengarTech <68355453+haircutfish@users.noreply.github.com>
2022-10-06 14:58:35 -0400

    Update Exercise map-filter-zip-reduce.py

--- Exercise map-filter-zip-reduce.py
+++ Exercise map-filter-zip-reduce.py
@@ -1,41 +1,40 @@
 from functools import reduce
 
-# 1 Capitalize all of the pet names and print the list
+#1 Capitalize all of the pet names and print the list
+def caps(item):
+    return item.title()
+
 my_pets = ['sisi', 'bibi', 'titi', 'carla']
 
+print(list(map(caps,my_pets)))
 
-def caps(item):
-    return item.upper()
+#2 Zip the 2 lists into a list of tuples, but sort the numbers from lowest to highest.
+sorted_list = []
 
-print(list(map(caps, my_pets)))
-
-# 2 Zip the 2 lists into a list of tuples, but sort the numbers from lowest to highest.
 my_strings = ['a', 'b', 'c', 'd', 'e']
-my_numbers = [5, 4, 3, 2, 1]
+my_numbers = [5,4,3,2,1]
 
-new_numbers = sorted(my_numbers, reverse=False)
+my_numbers.sort()
 
-print(list(zip(my_strings, new_numbers)))
-# 3 Filter the scores that pass over 50%
+print(list(zip(my_strings,my_numbers)))
+
+
+#3 Filter the scores that pass over 50%
 scores = [73, 20, 65, 19, 76, 100, 88]
 
-
-def above_ave(item):
+def over_fifty(item):
     return item > 50
 
+print(list(filter(over_fifty, scores)))
 
-print(list(filter(above_ave, scores)))
 
+#4 Combine all of the numbers that are in a list on this file using reduce (my_numbers and scores). What is the total?
 
-# 4 Combine all of the numbers that are in a list on this file using reduce (my_numbers and scores). What is the total?
+def combine(com, num):
+    # print(com, num)
+    return com + num
 
-new_list = list(set(my_numbers + scores))
-
-def accumulator(acc, item):
-    print(acc , item)
-    return acc + item
-
-print(reduce(accumulator, new_list, 0)) #456
+print(reduce(combine,my_numbers + scores,0)) #456
 
 
 
